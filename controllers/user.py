94cc26822a8941ca4920b15da72adf1d73470ca5
Nicolas <nicolasmanan21@gmail.com>
2017-04-30 15:47:37 -0400

    Se modificaron campos a no editables, vista perfil

--- controllers/user.py
+++ controllers/user.py
@@ -7,9 +7,14 @@ def profile():
     mensaje = 'Bienvenido ' + auth.user.first_name + ' ' + auth.user.last_name
     usuario = auth.user
 
-    form = SQLFORM(db.auth_user, fields = ['first_name', 'last_name', 'ci',
-                                           'username' ,'email', 'phone'],
-                   record = auth.user.id, showid = False, labels = {'username':'USBID', 'ci':'Cédula', 'phone':'Teléfono'})
+    db.auth_user.ci.writable = False
+    db.auth_user.username.writable = False
+    db.auth_user.email.writable = False
+    form = SQLFORM(db.auth_user, fields = ['first_name', 'last_name', 'phone',
+                                           'ci', 'username' ,'email'],
+                   record = auth.user.id, showid = False, formstyle = 'bootstrap',
+                   labels = {'username':'USBID', 'ci':'Cédula', 'phone':'Teléfono'})
+
     if form.process().accepted:
         response.flash = 'form accepted'
     elif form.errors:
