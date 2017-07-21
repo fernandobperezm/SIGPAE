4967e5c91883103adb3fb2b8acd130643784c08d
Leonardo Martinez <martinezazuaje@gmail.com>
2017-07-20 22:42:23 -0400

    URL de retorno para el servidor

--- models/0.py
+++ models/0.py
@@ -22,6 +22,6 @@ settings.plugins      = []
 # DESCOMENTAR LA URL DEPENDIENDO DE SI SE ESTA TRABAJANDO LOCALMENTE O ES
 # LA VERSION USADA EN EL SERVIDOR.
 # LOCAL:
-settings.returnurl = "http%3A%2F%2Flocalhost%3A8000%2FSIGPAE%2Fdefault%2Flogin_cas"
+#settings.returnurl = "http%3A%2F%2Flocalhost%3A8000%2FSIGPAE%2Fdefault%2Flogin_cas"
 # SERVIDOR:
-#settings.returnurl =
+settings.returnurl = "http%3A%2F%2F159.90.61.100%3A8000%2FSIGPAE%2Fdefault%2Flogin_cas"
