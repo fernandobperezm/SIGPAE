8ed748c5d9680a48d561110da0449805cce68ba7
Leonardo Martinez <martinezazuaje@gmail.com>
2017-09-19 14:37:59 -0400

    Restoring 0.py

---
+++ models/0.py
@@ -0,0 +1,27 @@
+from gluon.storage import Storage
+
+settings = Storage()
+
+settings.migrate = True
+settings.title = 'SIGPAE'
+settings.subtitle = 'Sistema de Gestión de Programas Analíticos de Estudio'
+settings.author   = 'Leonardo Martínez, Jonnathan Ng, Nicolás Mañan.'
+settings.author_email = ''
+settings.keywords     = ''
+settings.description  = ''
+settings.layout_theme = ''
+settings.database_uri = 'postgres://sigpae:123123@localhost/newsigpae'
+settings.login_method = 'local'
+settings.email_server = 'smtp.gmail.com:587'
+settings.email_sender = 'noreplysigpaeusb@gmail.com'
+settings.email_login  = 'noreplysigpaeusb@gmail.com:LXHyCmFD9rQPCqC'
+settings.login_config = ''
+settings.plugins      = []
+
+# CONFIGURACION DE URLS
+# DESCOMENTAR LA URL DEPENDIENDO DE SI SE ESTA TRABAJANDO LOCALMENTE O ES
+# LA VERSION USADA EN EL SERVIDOR.
+# LOCAL:
+settings.returnurl = "http%3A%2F%2Flocalhost%3A8000%2FSIGPAE%2Fdefault%2Flogin_cas"
+# SERVIDOR:
+#settings.returnurl = 
